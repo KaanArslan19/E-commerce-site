319895da60dbac0ca56565dc45490aa03bcba940
Kaan Arslan <kaanarslan35@gmail.com>
2023-01-26 21:36:41 +0300

    package error and some styles changed

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,35 +1,33 @@
-import Link from 'next/link'
-import React from 'react'
-import { urlFor } from '../lib/client'
+import Link from "next/link";
+import React from "react";
+import { urlFor } from "../lib/client";
 
-
-const HeroBanner = ({heroBanner}) => {
+const HeroBanner = ({ heroBanner }) => {
   return (
-    <div className='hero-banner-container'>
-    <div className="">
-      <div className="hero-banner-styling">
-      <div className="hero-banner-text">
-      <p className='tote-bag'>{heroBanner.smallText}</p>
-      <h3>{heroBanner.midText}</h3>
-      <h3>{heroBanner.largeText2}</h3>
-      </div>
-      <img src={urlFor(heroBanner.image)} alt="clothes" className='hero-banner-image' />
-      </div>
-      <div className='hero-second-row'>
-        <div>
-          
-        <Link href={`/product/${heroBanner.product}`}>
-          <button type="button">{heroBanner.buttonText}</button>
-        </Link>
+    <div className="hero-banner-container">
+      <div className="">
+        <div className="hero-banner-styling">
+          <div className="hero-banner-text">
+            <p className="tote-bag">{heroBanner.smallText}</p>
+            <h3>{heroBanner.midText}</h3>
+            <h3>{heroBanner.largeText2}</h3>
+          </div>
+          <img
+            src={urlFor(heroBanner.image)}
+            alt="clothes"
+            className="hero-banner-image"
+          />
         </div>
-        <div className="desc">
-          
-          <p>{heroBanner.desc}</p>
+        <div className="hero-second-row">
+          <div className="hero-button">
+            <Link href={`/product/${heroBanner.product}`}>
+              <button type="button">{heroBanner.buttonText}</button>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default HeroBanner
+export default HeroBanner;
